cede85479dce8d1ffca77d07f7c67b5cb67f7472
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-06-08 07:16:18 +0530

    double_equals_and_triple_equals

---
+++ JS question PG/double_equal_and_triple_equal.js
@@ -0,0 +1,10 @@
+// triple equal compares value and data types as well
+
+console.log(1 =='1') //true 
+console.log(1 === '1') // false 
+console.log(null == null) // true 
+console.log(null === null) // true 
+console.log(null == undefined) // true (only use case of ==)
+console.log(null === undefined) // false
+console.log(1 != '1') // false
+console.log(1 !== '1') // true 
