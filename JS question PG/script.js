e49bc0dd4745e5f3d8dd48871bda2c4f583e6349
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-06-08 06:46:24 +0530

    array methods

--- JS question PG/script.js
+++ JS question PG/script.js
@@ -16,13 +16,13 @@ const ans  = arr.map((x) =>{
     return x*2;
 } )
 
-console.log(ans);
+// console.log(ans);
 
 const itemMap = items.map((item)=>{
     return item.name;
 })
 
-console.log("itemMap" , itemMap);
+// console.log("itemMap" , itemMap);
 
 //________________________________________
               // FILTER //
@@ -32,17 +32,57 @@ function sortAge(age){
     return age <30;
 }
 const ans2 = age.filter(sortAge);
-console.log(ans2);
+// console.log(ans2);
 
 const ans3 =  age.filter((age)=>{
     return age >40;
 })
-console.log(ans3)
+// console.log(ans3)
 
 
 const ans4 =  items.filter((item)=>{
     return item.price > 50;
 })
 
-console.log(ans4);
+// console.log(ans4);
 //________________________________________
+                  // FIND
+ const ans5 = items.find((item)=>{
+    return item.price === 20
+ })                 
+
+
+ console.log(ans5);
+
+
+ //______________________________________________
+                   //For each (sae as for loop
+   items.forEach((item)=>{
+    console.log(item);
+   })    
+   
+   //_______________________________________________
+   // Some method ===> return tus the true
+   // lets sat we have to check wehther an items is present below 100 or not
+
+   const hasItems =  items.some((item)=>{
+    return item.price <= 100;
+   })
+
+   console.log(hasItems)
+//____________________________________________________
+                    // Reduce
+// used to perform multiple operation like we have to calculate the sume of proces 
+var prices =  0;
+items.forEach((item)=>{
+    prices =  prices + item.price;
+})
+
+console.log(prices);
+
+const total = items.reduce((currentTotal , item)=>{
+    return currentTotal+item.price
+} ,  0)
+
+console.log(total);
+            
